4e853dc982fcab239e0d8a16c7076fbdfdcf0f2f
Mario Milosevic <128989997+MarioMilosevic@users.noreply.github.com>
2024-03-18 09:40:58 +0100

    Add options

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
 function App() {
   return (
     <>
-      <div className="border border-black">Mario</div>
+      <div className="border border-black">
+        <h1>Quiz setup</h1>
+        <label htmlFor="questions">Number of questions</label>
+        <input type="number" value="10" />
+
+        <label htmlFor="">Category</label>
+        <select name="category" id="category">
+          <option value="Mythology">Mythology</option>
+          <option value="General Knowledge">General Knowledge</option>
+          <option value="Video Games">Video Games</option>
+          <option value="Sports">Sports</option>
+        </select>
+        <label htmlFor="difficulty">Difficulty</label>
+        <select name="difficulty" id="difficulty">
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="Hard">Hard</option>
+        </select>
+      </div>
     </>
   );
 }
